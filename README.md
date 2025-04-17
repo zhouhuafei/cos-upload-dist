e7443be608b4631e5517612ca56d35277015a97b
zhouhuafei <1123486116@qq.com>
2025-04-17 18:36:08 +0800

    feat: 🎸 把静态资源上传到腾讯云进行存储

--- README.md
+++ README.md
@@ -6,3 +6,31 @@ git remote set-url origin git@github.com:zhouhuafei/cos-upload-dist.git
 git pull -p
 git push
 ```
+
+## 把静态资源上传到腾讯云进行存储
+```javascript
+const { fnInit, fnUploadFiles } = require('cos-upload-dist')
+
+console.log('当前运行目录:', process.cwd())
+
+fnInit({
+  SecretId: '', // 腾讯云子账户或主账户的 SecretId
+  SecretKey: '', // 腾讯云子账户或主账户的 SecretKey
+  Bucket: '', // 填入您自己的存储桶，必须字段
+  Region: '' // 存储桶所在地域，例如 ap-beijing，必须字段
+})
+
+fnUploadFiles({
+  // fast-glob的配置
+  fastGlobConfig: [
+    // ['./dist/**/*.*', '!./dist/**/*.html'], // 上传dist目录中所有的文件，除了以.html结尾的文件。
+    ['./dist/**/*.*'], // 上传dist目录中所有的文件。
+    { dot: true } // 使之支持上传dist目录中，以.开头的文件，例如.editorconfig文件。
+  ],
+  // 以 dist 目录中的 css/app.19a8a3b7.css 文件为例
+  // 如果 pathPrefix 为 '' 则文件的存储路径为 css/app.19a8a3b7.css
+  // 如果 pathPrefix 为 'project1' 则文件的存储路径为 project1/css/app.19a8a3b7.css
+  // 如果 pathPrefix 为 'project1/dist' 则文件的存储路径为 project1/dist/css/app.19a8a3b7.css
+  pathPrefix: 'project1/dist'
+})
+```
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,49 @@
-import { sum } from '@/utils/sum'
+import fg from 'fast-glob'
+import COS from 'cos-nodejs-sdk-v5'
 
-export { sum }
+interface UploadConfig {
+  fastGlobConfig: [string[], fg.Options]
+  pathPrefix: string
+}
+
+let cos = null
+let cosOptions = null
+
+export const fnInit = async (options) => {
+  // console.log('fnInit options：', options)
+
+  cos = new COS({
+    SecretId: options.SecretId,
+    SecretKey: options.SecretKey
+  })
+
+  cosOptions = options
+
+  return { cos, cosOptions }
+}
+
+export const fnUploadFiles = async (options: UploadConfig) => {
+  // console.log('fnUploadFiles options：', options)
+  const entries = await fg(...options.fastGlobConfig)
+
+  entries.forEach(localFilePath => {
+    // console.log('localFilePath：', localFilePath)
+    const arr = localFilePath.split('/').filter(v => v !== '.' && v !== '..')
+    if (options.pathPrefix) arr[0] = options.pathPrefix
+    const remoteFilePath = arr.join('/')
+    // console.log('remoteFilePath：', remoteFilePath)
+
+    cos.uploadFile({
+      Bucket: cosOptions.Bucket, // 填入您自己的存储桶，必须字段
+      Region: cosOptions.Region, // 存储桶所在地域，例如 ap-beijing，必须字段
+      Key: remoteFilePath, // 存储在桶里的对象键（例如1.jpg，a/b/test.txt），必须字段
+      FilePath: localFilePath // 必须
+    }, (error, data) => {
+      if (error) {
+        console.log('上传失败：', localFilePath, '=>', remoteFilePath)
+      } else {
+        console.log('上传成功：', localFilePath, '=>', remoteFilePath)
+      }
+    })
+  })
+}
